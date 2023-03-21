40d2405cba0ec19078d384868062964ac17d7277
Emmanuel <babatundeolaemma@gmail.comm>
2023-03-21 04:00:02 +0100

    first commit

---
+++ operators.js
@@ -0,0 +1,45 @@
+let num_1 = 10;
+let num_2 = 5;
+console.log (num_1 + num_2)
+
+// Addition
+console.log ("num_1 + num_2 is:" , num_1 + num_2)
+// Subtraction
+console.log ("num_1 - num_2 is:" , num_1 - num_2)
+// Multiplication
+console.log ("num_1 * num_2 is:" , num_1 * num_2)
+// 	Division
+console.log ("num_1 / num_2 is:" , num_1 / num_2)
+// Remainder
+console.log ("num_1 % num_2 is:" , num_1 % num_2)
+// Increment
+num_1 += 1
+console.log ("num_1 +=1 is:", num_1)
+// Decrement
+num_2 -= 1
+console.log ("num_2 -=1 is:", num_2)
+
+const input_1 = prompt ("Please, Enter your first input");
+const input_2 = prompt ("Please, Enter your second input");
+
+
+parseFloat(input_1);
+parseFloat(input_2);
+
+// Addition
+console.log ("input_1 + input_2 is:" , input_1 + input_2)
+// Subtraction
+console.log ("input_1 - input_2 is:" , input_1 - input_2)
+// Multiplication
+console.log ("input_1 * input_2 is:" , input_1 * input_2)
+// 	Division
+console.log ("input_1 / input_2 is:" , input_1 / input_2)
+// Remainder
+console.log ("input_1 % input_2 is:" , input_1 % input_2)
+// Increment
+input_1 += 1
+console.log ("input_1 +=1 is:", input_1)
+// Decrement
+input_2 -= 1
+console.log ("input_2 -=1 is:", input_2)
+
